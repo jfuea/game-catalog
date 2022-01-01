0cde419f25e7fc6d64de41e7137cd6ec2dd57911
Josue Fuentes <fuentesallauca@gmail.com>
2022-01-01 09:27:07 -0500

    dynamic content for game-tabs.component

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,13 +1,19 @@
 export interface Game {
     id: string;
     background_image: string;
-    name: string;
-    released: string;
-    metacritic_url: string;
-    website: string;
-    parent_platforms: Array<ParentPlatform>;
+    description: string;
     genres: Array<Genre>;
+    metacritic_url: string;
     metacritic: number;
+    name: string;
+    parent_platforms: Array<ParentPlatform>;
+    platforms: Array<Platform>;
+    publishers: Array<Publisher>;
+    released: string;
+    website: string;
+    ratings: Array<Rating>;
+    screenshots: Array<Screenshot>;
+    trailers: Array<Trailer>;
 }
 
 export interface APIResponse<T> {
@@ -16,14 +22,56 @@ export interface APIResponse<T> {
 }
 
 export interface ParentPlatform {
-    platform: {
-        name: string;
-        slug: string;
-    }
+    platform: PlatformContent;
 }
 
 export interface Genre {
     id: number;
     name: string;
     slug: string;
+}
+
+export interface Platform {
+    platform: PlatformContent;
+    released_at: string;
+}
+
+export interface PlatformContent {
+    games_count: number;
+    id: number;
+    image: string;
+    image_background: string;
+    name: string;
+    slug: string;
+    year_end: number;
+}
+
+export interface Publisher {
+    games_count: number;
+    id: number;
+    image_background: string;
+    name: string;
+    slug: string;
+}
+
+export interface Rating {
+    id: number;
+    count: number;
+    percent: number;
+    title: string;
+}
+
+export interface Screenshot {
+    id: number;
+    height: number;
+    image: string;
+    is_deleted: boolean;
+    width: number;
+}
+
+export interface Trailer {
+    id: number;
+    name: string;
+    preview: string;
+    data: object;
 }
